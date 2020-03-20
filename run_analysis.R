4ea5f8df32463e1c5ad5e7a1300055989eb14f5c
dido0320 <60595806+dido0320@users.noreply.github.com>
2020-03-20 10:15:12 +0800

    Update and rename  run_analysis.R to run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,51 +1,51 @@
-#read subjects
+#Read subjects
 path <- getwd()
 d <- 'UCI HAR Dataset'
 DT1<-data.table(read.table(file.path(path, d, 'train', 'subject_train.txt')))
 DT2 <- data.table(read.table(file.path(path, d, 'test', 'subject_test.txt')))
 DT3 <- rbind(DT1, DT2)
 
-#read activities
+#Read activities
 DAT1<- data.table(read.table(file.path(path, d, 'train','Y_train.txt')))
 DAT2<-data.table(read.table(file.path(path, d, 'test','Y_test.txt')))
 DAT3<- rbind(DAT1,DAT2)
 names(DT3)<- c('subject')
 names(DAT3)<-c('activity')
 
-#combine subject and activity
+#Combine subject and activity
 SA<- cbind(DT3,DAT3)
 
-#read datasets
+#Read datasets
 Train.set1 <- data.table(read.table(file.path(path,d,'train','X_train.txt')))
 Train.set2 <- data.table(read.table(file.path(path,d,'test','X_test.txt')))
 Train.set3<- rbind(Train.set1,Train.set2)
 
-#combine subject,activity and datasets
+#Combine subject,activity and datasets
 TEST<- cbind(SA,Train.set3)
 
-# read feature 
+#Read feature 
 feature <- data.table(read.table(file.path(path,d,'features.txt'))) 
 names(feature) <- c('index','value')
 featurename <- feature [,2]
 
-# extract mean and std
+#Extract mean and std
 featureIndex <- grep(("mean\\(\\))|std\\(\\)"), featurename$value)
 finalData <- select(TEST,c(1,2,featureIndex+2))
 b<- c(featurename[featureIndex])
 colnames(finalData) <- c("subject", "activity", as.character(b$value))
 
-#Uses descriptive activity names to name the activities in the data set
+#Use descriptive activity names to name the activities in the data set
 TESTnames<-data.table(read.table(file.path(path, d, 'activity_labels.txt')))
 names(TESTnames ) <- c('activity','activity_type')
 
-#Appropriately labels the data set with descriptive variable names.
+#Appropriately label the data set with descriptive variable names.
 names(finalData) <- gsub("\\()", "", names(finalData))
 names(finalData) <- gsub("^t", "time", names(finalData))
 names(finalData) <- gsub("^f", "frequence", names(finalData))
 names(finalData) <- gsub("-mean", "Mean", names(finalData))
 names(finalData) <- gsub("-std", "Std", names(finalData))
 
-#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.H
+#Create a second, independent tidy data set with the average of each variable for each activity and each subject.H
 AT<-merge(TESTnames,finalData,by = "activity")
 AT_Tidy<- AT %>% group_by(subject, activity_type)  %>% summarise_each(funs(mean)) %>% select(-activity)
 
